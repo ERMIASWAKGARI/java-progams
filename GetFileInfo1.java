6c80ef894206e552e19c07a824e03e5fd37e3acb
Ermias Wakgari Yadeta <118937954+ERMIASWAKGARI@users.noreply.github.com>
2023-01-22 18:13:07 +0300

    Add files via upload

---
+++ GetFileInfo1.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.getfileinfo;
+
+import java.io.File;
+
+/**
+ *
+ * @author Ermix
+ */
+public class GetFileInfo1 {
+
+ 
+  public static void main(String[] args) {
+    File myObj = new File("filename.txt");
+    if (myObj.exists()) {
+      System.out.println("File name: " + myObj.getName());
+      System.out.println("Absolute path: " + myObj.getAbsolutePath());
+      System.out.println("Writeable: " + myObj.canWrite());
+      System.out.println("Readable " + myObj.canRead());
+      System.out.println("File size in bytes " + myObj.length());
+    } else {
+      System.out.println("The file does not exist.");
+    }
+  }
+
+}
+
