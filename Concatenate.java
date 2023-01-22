21e6c27a9cec18dacb2276ca8a9ed225d6fc8819
Ermias Wakgari Yadeta <118937954+ERMIASWAKGARI@users.noreply.github.com>
2023-01-22 18:01:23 +0300

    Add files via upload

---
+++ Concatenate.java
@@ -0,0 +1,12 @@
+
+
+package com.mycompany.concatenate;
+public class Concatenate {
+int checkConcatenatedSum(int n, int catlen){
+   for(int i=0;i<;i++) 
+    if(n==3)
+} 
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
