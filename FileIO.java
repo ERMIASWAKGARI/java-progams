28c1500739abfa4d3584b9f59d8a515941592422
Ermias Wakgari Yadeta <118937954+ERMIASWAKGARI@users.noreply.github.com>
2023-01-22 18:11:56 +0300

    Add files via upload

---
+++ FileIO.java
@@ -0,0 +1,11 @@
+
+
+package com.mycompany.fileio;
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.IOException;
+public class FileIO {
+
+    public static void main(String[] args) {
+        
+}
